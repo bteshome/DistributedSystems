e33a2c7e9e11befa5802e81db661caaf0c6cd026
tesh <bttesh24@gmail.com>
2025-03-11 15:44:17 -0400

    added authentication and ingress to kvs admin and os admin

---
+++ online-store/admin-dashboard/src/main/java/com/bteshome/onlinestore/admindashboard/common/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.bteshome.onlinestore.admindashboard.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests( authorize -> authorize
+                        .requestMatchers("/**").authenticated())
+                .oauth2Login(httpSecurityOAuth2LoginConfigurer -> {})
+                .build();
+    }
+}
